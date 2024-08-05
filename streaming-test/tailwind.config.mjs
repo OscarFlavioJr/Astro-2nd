2bda557d1ab037b840c1905d1609ed4c1d534dd4
Oscar_Flavio_Jr <oscarflaviojunior@gmail.com>
2024-08-05 15:51:21 -0300

    Não consegui fazer do jeito que quero ainda!

--- streaming-test/tailwind.config.mjs
+++ streaming-test/tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',],
 	theme: {
-		extend: {},
+		extend: {
+			colors : {
+				light: '#999999',
+				dark: '#313131'
+			},
+			fontFamily: {
+				'sans' : ['Montserrat' , 'ui-sans-serif' , 'system-ui']
+			}
+		},
 	},
 	plugins: [],
 }
